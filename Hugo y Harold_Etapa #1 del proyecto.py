7555319a4ec1f85362742b2498843c62c98b6f9a
Harold <61435652+HaroldERM@users.noreply.github.com>
2020-05-24 15:35:18 -0600

    Primer commit(login)

--- Hugo y Harold_Etapa #1 del proyecto.py
+++ Hugo y Harold_Etapa #1 del proyecto.py
@@ -1,4 +1,22 @@
-def c():
-    print("hola")
-    print('mundo')
-    print("lalala")
+def login():
+    comando = 0
+    while comando < 4 :
+        print("\n*********************************************************")
+        print("*                                                       *") 
+        print("* Digite:  1      para: ingresar como Administrador     *")   
+        print("* Digite:  2      para: ingresar como Analista          *")
+        print("* Digite:  3      para: ingresar como Usuario           *") 
+        print("* Digite: #>3:    para Salir                            *")  
+        print("*                                                       *")  
+        print("*********************************************************\n")
+        comando=int(input("Digite un numero correspondiente: "))
+        if comando < 4 :
+            if comando == 1 :
+                print("\nEsta en usuario Administrador")
+            elif comando == 2 :
+                print("\nEsta en usuario Analista")
+            elif comando == 3 :
+                print("\nEsta en usuario Usuario")
+        else :
+            print("Chao")
+login()    
