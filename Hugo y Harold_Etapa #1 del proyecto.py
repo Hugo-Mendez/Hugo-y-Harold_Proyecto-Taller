e00002b2a1aae0939680b5b1554194b5b41de3d6
Harold <61435652+HaroldERM@users.noreply.github.com>
2020-05-29 16:58:55 -0600

    Cambio de estructuras para almacenar los atributos(de listas a diccionarios).

--- Hugo y Harold_Etapa #1 del proyecto.py
+++ Hugo y Harold_Etapa #1 del proyecto.py
@@ -3,41 +3,44 @@ from datetime import date
 
 
 def Crea_cedulas(cantidad):
-    cedulas = random.sample(list(range(200000000,209999999)), cantidad)
-    return cedulas
+    Cedulas = {}
+    for i in range(1,cantidad+1):
+        cedula = random.sample(range(200000000,209999999), cantidad)
+        Cedulas[i] = cedula[i-1]
+    return Cedulas
  
 def Crea_provincias():
-    provincias = ["San José  ", "Alajuela  ", "Cartago   ", "Heredia   ", "Puntarenas", "Guanacaste", "Limón     "]
+    provincias = {1: "San José  ", 2: "Alajuela  ", 3: "Cartago   ", 4: "Heredia   ", 5: "Puntarenas", 6: "Guanacaste", 7: "Limón     "}
     return provincias
 
 def Crea_accesorios():
-    accesorios = ["Lentes  ", "Aretes  ", "Piercing", "Ninguno "]
+    accesorios = {1: "Lentes  ", 2: "Aretes  ", 3: "Piercing", 4: "Ninguno "}
     return accesorios
 
 def Crea_genero():
-    genero = ["Femenino ", "Masculino"]
+    genero = {1: "Femenino ", 2: "Masculino"}
     return  genero
 
 def Crea_color_piel():
-    colorpiel = ["Negra ", "Marrón", "Morena", "Clara ", "Blanca"]
+    colorpiel = {1: "Negra ", 2: "Marrón", 3: "Morena", 4: "Clara ", 5: "Blanca"}
     return colorpiel
 
 def Crea_rostro():
-    rostro = ["Redondo    ", "Alargado   ", "Corazón    ", "Cuadrado   ", "Ovalado    ", "Rectangular"]
+    rostro = {1: "Redondo    ", 2: "Alargado   ", 3: "Corazón    ", 4: "Cuadrado   ", 5: "Ovalado    ", 6: "Rectangular"}
     return rostro
 
 def Crea_emociones():
-    emociones = ["Enfado   ", "Desprecio", "Disgusto ", "Miedo    ", "Sorpresa ", "Alegría  ", "Neutral  ", "Tristeza "]
+    emociones = {1: "Enfado   ", 2: "Desprecio", 3: "Disgusto ", 4: "Miedo    ", 5: "Sorpresa ", 6: "Alegría  ", 7: "Neutral  ", 8: "Tristeza "}
     return emociones
 
 def Crea_Atributos_Cabello():
-    Atr_cabello = [{1:"Negro    ", 2:"Rubio    ", 3:"Café     ", 4:"Castaño  ", 5:"Gris     ", 6:"Invisible"},
-                   {1:"Escaso   ", 2:"Moderado ", 3:"Abundante"}, {1:"Lacio   ", 2:"Ondulado", 3:"Rizado  "}]
+    Atr_cabello = {1: {1:"Negro    ", 2:"Rubio    ", 3:"Café     ", 4:"Castaño  ", 5:"Gris     ", 6:"Invisible"},
+                   2: {1:"Escaso   ", 2:"Moderado ", 3:"Abundante"}, 3: {1:"Lacio   ", 2:"Ondulado", 3:"Rizado  "}}
     return Atr_cabello
 
 def Crea_Atributos_Ojos():
-    Atr_ojos = [{1: "Almendrados", 2: "Separados  ", 3: "Redondos   ", 4: "Caídos     ", 5: "Saltones   ", 6: "Juntos     ", 7: "Profundos  ", 8: "Asiáticos  "},
-                {1: "Negro   ", 2: "Castaño ", 3: "Ámbar   ", 4: "Avellana", 5: "Verde   ", 6: "Azul    ", 7: "Gris    "}]         
+    Atr_ojos = {1: {1: "Almendrados", 2: "Separados  ", 3: "Redondos   ", 4: "Caídos     ", 5: "Saltones   ", 6: "Juntos     ", 7: "Profundos  ", 8: "Asiáticos  "},
+                2: {1: "Negro   ", 2: "Castaño ", 3: "Ámbar   ", 4: "Avellana", 5: "Verde   ", 6: "Azul    ", 7: "Gris    "}}       
     return Atr_ojos
 
 def Crea_Fecha_Nac():
@@ -62,36 +65,35 @@ def Calcula_Edad(Fecha_na):
 
 def Crea_Personas(cedulas,provincias,accesorios,genero,colores_de_piel,rostro,emociones,atri_cabello,atri_ojos):
     Lista_Personas = []
-    for i in cedulas:
+    for i in range(1,len(cedulas)+1):
         Fecha_na = Crea_Fecha_Nac()
         Edad_Años = Calcula_Edad(Fecha_na)
-        dic_persona = {1: i, 2: random.choice(provincias), 3: random.choice(accesorios),
-                       4: random.choice(genero), 5: random.choice(colores_de_piel),
-                       6: random.choice(rostro), 7: random.choice(emociones),
-                       8: atri_cabello[0][random.randint(1,6)], 9: atri_cabello[1][random.randint(1,3)],
-                       10: atri_cabello[2][random.randint(1,3)], 11: atri_ojos[0][random.randint(1,8)], 
-                       12: atri_ojos[1][random.randint(1,7)], 13: Edad_Años}
+        dic_persona = {1: cedulas[random.randint(1,len(cedulas))], 2: provincias[random.randint(1,len(provincias))], 3: accesorios[random.randint(1,len(accesorios))],
+                       4: genero[random.randint(1,len(genero))], 5: colores_de_piel[random.randint(1,len(colores_de_piel))],
+                       6: rostro[random.randint(1,len(rostro))], 7: emociones[random.randint(1,len(emociones))],
+                       8: atri_cabello[1][random.randint(1,6)], 9: atri_cabello[2][random.randint(1,3)],
+                       10: atri_cabello[3][random.randint(1,3)], 11: atri_ojos[1][random.randint(1,8)], 
+                       12: atri_ojos[2][random.randint(1,7)], 13: Edad_Años}
         Lista_Personas.append(dic_persona)
     return Lista_Personas
 
-
 def Crear_Persona_Auto(Personas,nueva_cedula,cedulas,provincias,accesorios,genero,colores_de_piel,rostro,emociones,atri_cabello,atri_ojos):
-    while nueva_cedula[0] in cedulas:
+    while nueva_cedula[1] in cedulas:
         nueva_cedula = Crea_cedulas(1)
     Fecha_na = Crea_Fecha_Nac()
     Edad_Años = Calcula_Edad(Fecha_na)
-    dic_persona = {1: nueva_cedula, 2: random.choice(provincias), 3: random.choice(accesorios),
-                    4: random.choice(genero), 5: random.choice(colores_de_piel),
-                    6: random.choice(rostro), 7: random.choice(emociones),
-                    8: atri_cabello[0][random.randint(1,6)], 9: atri_cabello[1][random.randint(1,3)],
-                    10: atri_cabello[2][random.randint(1,3)], 11: atri_ojos[0][random.randint(1,7)],
-                    12: atri_ojos[1][random.randint(1,8)], 13: Edad_Años}
+    dic_persona = {1: nueva_cedula[1], 2: provincias[random.randint(1,len(provincias))], 3: accesorios[random.randint(1,len(accesorios))],
+                   4: genero[random.randint(1,len(genero))], 5: colores_de_piel[random.randint(1,len(colores_de_piel))],
+                   6: rostro[random.randint(1,len(rostro))], 7: emociones[random.randint(1,len(emociones))],
+                   8: atri_cabello[1][random.randint(1,6)], 9: atri_cabello[2][random.randint(1,3)],
+                   10: atri_cabello[3][random.randint(1,3)], 11: atri_ojos[1][random.randint(1,8)], 
+                   12: atri_ojos[2][random.randint(1,7)], 13: Edad_Años}
     Personas.append(dic_persona)
     print("\nPersona creada y agregada a la lista satisfactoriamente")
     return
 
 def Crear_Persona_Manual(Personas,nueva_cedula,cedulas,provincias,accesorios,genero,colores_de_piel,rostro,emociones,atri_cabello,atri_ojos):
-    while nueva_cedula[0] in cedulas:
+    while nueva_cedula[1] in cedulas:
         nueva_cedula = Crea_cedulas(1)
     print("\nPara crear una persona manualmente por favor indica cada uno de los siguientes atributos:")
 
@@ -288,16 +290,15 @@ def Crear_Persona_Manual(Personas,nueva_cedula,cedulas,provincias,accesorios,gen
             print("\nDigito incorrecto, por favor digite un número correspondiente con el menú.") 
     
     Edad_Años = Calcula_Edad(fecha_naci)
-    dic_persona = {1: nueva_cedula[0], 2: provincias[provincia-1], 3: accesorios[accesorio-1],
-                   4: genero[Genero-1], 5: colores_de_piel[Color_piel-1],
-                   6: rostro[F_rostro-1], 7: emociones[emocion-1],
-                   8: atri_cabello[0][color_cabello], 9: atri_cabello[1][densidad_cabello],
-                   10: atri_cabello[2][textura_cabello], 11: atri_ojos[0][Forma_ojos], 
-                   12: atri_ojos[1][Color_ojos], 13: Edad_Años}
+    dic_persona = {1: nueva_cedula[1], 2: provincias[provincia], 3: accesorios[accesorio],
+                   4: genero[Genero], 5: colores_de_piel[Color_piel],
+                   6: rostro[F_rostro], 7: emociones[emocion],
+                   8: atri_cabello[1][color_cabello], 9: atri_cabello[2][densidad_cabello],
+                   10: atri_cabello[3][textura_cabello], 11: atri_ojos[1][Forma_ojos], 
+                   12: atri_ojos[2][Color_ojos], 13: Edad_Años}
     Personas.append(dic_persona)
     print(Personas)
     return
-
 
 def Administrador(Personas,cedulas,provincias,accesorios,genero,colores_de_piel,rostro,emociones,atri_cabello,atri_ojos):
     comando = 0
@@ -317,6 +318,7 @@ def Administrador(Personas,cedulas,provincias,accesorios,genero,colores_de_piel,
     return
 
 def Impresion_Datos_Usuario(usuario,mensaje):
+    
     print("\n****************************************************************************")
     print("*                      ",mensaje,"                *")
     print("****************************************************************************")
@@ -350,7 +352,7 @@ def Modificar_emocion(usuario,emociones):
         emocion = int(input("Digite un número correspondiente con el menú: "))
         if emocion not in range(1,9):
             print("\nDigito incorrecto, por favor digite un número correspondiente con el menú.")
-    usuario[7] = emociones[emocion-1]
+    usuario[7] = emociones[emocion]
     Impresion_Datos_Usuario(usuario,"Ahora esta es su nueva información")
     return 
 
@@ -372,7 +374,7 @@ def Modificar_provincia(usuario, provincias):
         provincia = int(input("Digite un número correspondiente con el menú: "))
         if provincia not in range(1,8):
             print("\nDigito incorrecto, por favor digite un número correspondiente con el menú.")
-    usuario[2] = provincias[provincia-1]
+    usuario[2] = provincias[provincia]
     Impresion_Datos_Usuario(usuario,"Ahora esta es su nueva información")
     return
 
@@ -398,11 +400,8 @@ def Usuario(Personas,emociones,provincias):
     return
 
 def validar_contraseña(contraseña,comando,Personas) :
-    contraseña_Admin = "admi123"
-    contraseña_Analista = "ana456"
-    contraseña_Usuario = "usu789"
     if comando == 1 :        
-        while contraseña != contraseña_Admin :
+        while contraseña != "admi123" :
             contraseña = input("\nContraseña incorrecta: vuelva a digitar su contraseña o digite: 0 para regresar ")
             if contraseña == "0" :
                 return
@@ -412,14 +411,14 @@ def validar_contraseña(contraseña,comando,Personas) :
                       Crea_Atributos_Cabello(), Crea_Atributos_Ojos())
 
     elif comando == 2 :
-        while contraseña != contraseña_Analista :
+        while contraseña != "ana456" :
             contraseña = input("\nContraseña incorrecta: vuelva a digitar su contraseña o digite: 0 para regresar ")
             if contraseña == "0" :
                 return
         print("\nIngresó como Analista")
 
     else :
-        while contraseña != contraseña_Usuario :
+        while contraseña != "usu789" :
             contraseña = input("\nContraseña incorrecta: vuelva a digitar su contraseña o digite: 0 para regresar ")
             if contraseña == "0" :
                 return
