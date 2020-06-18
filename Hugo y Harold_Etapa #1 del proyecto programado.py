3bdce147dc92fb9a7136ad3efd54372f19f6fb84
Harold <61435652+HaroldERM@users.noreply.github.com>
2020-06-17 21:16:34 -0600

    Creacion de clases: Vestuario, Calzado, Ropa, Accesorios. Y sus respectivoos objetos. import copy

--- Hugo y Harold_Etapa #1 del proyecto programado.py
+++ Hugo y Harold_Etapa #1 del proyecto programado.py
@@ -1,7 +1,7 @@
 
 #Authors: Hugo Méndez and Harold Ramírez
 #Date: June 14th 2020
-
+import copy
 import random
 from datetime import date
 
@@ -153,6 +153,58 @@ class Ojos:
     def get_Color(self):
         return self.Color
 
+class Accesorios:
+    def ___init__(self):
+        self.Nombre = ""
+        return
+    def set_Accesorio(self, nombre):
+        self.Nombre = nombre
+        return
+    def get_Accesorio(self):
+        return self.Nombre
+
+class Ropa:
+    def ___init__(self):
+        self.Nombre = ""
+        return
+    def set_Ropa(self, nombre):
+        self.Nombre = nombre
+        return
+    def get_Ropa(self):
+        return self.Nombre
+
+class Calzado:
+    def ___init__(self):
+        self.Nombre = ""
+        return
+    def set_Calzado(self, nombre):
+        self.Nombre = nombre
+        return
+    def get_Calzado(self):
+        return self.Nombre
+       
+class Vestuario:
+    def __init__(self):
+        self.Ropa = []
+        self.Calzado = Calzado()
+        self.Accesorios = [] 
+        return
+    def set_Ropa(self, lista_Rop):
+        self.Ropa = lista_Rop
+        return
+    def set_Calzado(self, calzado):
+        self.Calzado = calzado.get_Calzado()
+        return
+    def set_Accesorios(self, lista_Acc):
+        self.Accesorios = lista_Acc
+        return
+    def get_Ropa(self):
+        return self.Ropa
+    def get_Calzado(self):
+        return self.Calzado
+    def get_Accesorios(self):
+        return self.Accesorios
+
 def Crea_cedulas(cantidad):
     """    Function that creates a dictionary, then, through a "for" loop: creates a list of ID cards and adds them to the dictionary.
     After, returns the dictionary.
@@ -175,10 +227,27 @@ def Crea_provincias():
         Obj_provincia[j].set_Nombre(provincias[j])  
     return Obj_provincia
 
-def Crea_accesorios():
+def Crea_vestuario():
     """Function that creates a dictionary which stores the accessories, then returns the dictionary."""
-    accesorios = {1: "Lentes  ", 2: "Aretes  ", 3: "Piercing", 4: "Ninguno "}
-    return accesorios
+    vestuarios = {1: {1: "Lentes  ", 2: "Aretes  ", 3: "Piercing", 4: "Collar", 5: "Anillo", 6: "Reloj", 
+                  7: "Brazalete", 8: "Diadema", 9: "Vincha", 10: "Monóculo"}, 
+                  2: {1: "Camiseta", 2: "Camisa", 3: "Blusa", 4: "Pantalón", 5: "Falda", 6: "Abrigo", 7: "Vestido",
+                  8: "Calcetines", 9: "Pantaloneta", 10: "Pantalones cortos", 11: "Saco", 12: "Corbata", 13: "Sombrero",
+                  14: "Bufanda" }, 3: {1: "Oxford", 2: "Sandalias", 3: "Tenis", 4: "Mocasines", 5: "Botas",
+                  6: "Botines de vestir", 7: "Brogue", 8: "Monk", 9: "Derby", 10: "Tacones"}}
+    Obj_vestuarios = copy.deepcopy(vestuarios)
+    for i in range(1, len(vestuarios)+1):
+        for e in range(1, len(vestuarios[i])+1):
+            if i == 1:
+                Obj_vestuarios[i][e] = Accesorios()
+                Obj_vestuarios[i][e].set_Accesorio(vestuarios[i][e])
+            elif i == 2:
+                Obj_vestuarios[i][e] = Ropa()
+                Obj_vestuarios[i][e].set_Ropa(vestuarios[i][e])
+            else:
+                Obj_vestuarios[i][e] = Calzado()
+                Obj_vestuarios[i][e].set_Calzado(vestuarios[i][e])
+    return Obj_vestuarios
 
 def Crea_genero():
     """Function that creates a dictionary which stores the two possible genres (female or male), then returns the dictionary."""
@@ -223,8 +292,7 @@ def Crea_Atributos_Cabello():
     Atr_cabello = {1: {1:"Negro    ", 2:"Rubio    ", 3:"Café     ", 4:"Castaño  ", 5:"Gris     ", 6:"Invisible"},
                    2: {1:"Escaso   ", 2:"Moderado ", 3:"Abundante"}, 3: {1:"Lacio   ", 2:"Ondulado", 3:"Rizado  "}}    
 
-    Obj_cabello = {1: {1:"Negro    ", 2:"Rubio    ", 3:"Café     ", 4:"Castaño  ", 5:"Gris     ", 6:"Invisible"},
-                   2: {1:"Escaso   ", 2:"Moderado ", 3:"Abundante"}, 3: {1:"Lacio   ", 2:"Ondulado", 3:"Rizado  "}}
+    Obj_cabello = copy.deepcopy(Atr_cabello)
     for i in range(1, len(Atr_cabello)+1):
         for j in range(1, len(Atr_cabello[i])+1):
             if i == 1:
@@ -235,7 +303,7 @@ def Crea_Atributos_Cabello():
                 Obj_cabello[i][j].set_Densidad(Atr_cabello[i][j])
             else:
                 Obj_cabello[i][j] = Textura_cabello()
-                Obj_cabello[i][j].set_Textura(Atr_cabello[i][j])
+                Obj_cabello[i][j].set_Textura(Atr_cabello[i][j])          
     return Obj_cabello 
 
 def Crea_Atributos_Ojos():
@@ -245,8 +313,7 @@ def Crea_Atributos_Ojos():
     """
     Atr_ojos = {1: {1: "Almendrados", 2: "Separados  ", 3: "Redondos   ", 4: "Caídos     ", 5: "Saltones   ", 6: "Juntos     ", 7: "Profundos  ", 8: "Asiáticos  "},
                 2: {1: "Negro   ", 2: "Castaño ", 3: "Ámbar   ", 4: "Avellana", 5: "Verde   ", 6: "Azul    ", 7: "Gris    "}}
-    Obj_ojos = {1: {1: "Almendrados", 2: "Separados  ", 3: "Redondos   ", 4: "Caídos     ", 5: "Saltones   ", 6: "Juntos     ", 7: "Profundos  ", 8: "Asiáticos  "},
-                2: {1: "Negro   ", 2: "Castaño ", 3: "Ámbar   ", 4: "Avellana", 5: "Verde   ", 6: "Azul    ", 7: "Gris    "}}  
+    Obj_ojos = copy.deepcopy(Atr_ojos) 
     for i in range(1, len(Obj_ojos)+1):
         for j in range(1, len(Obj_ojos[i])+1):
             if i == 1:
@@ -278,7 +345,7 @@ def Calcula_Edad(Fecha_na):
         Años = Años - 1 
     return Años
 
-def Crea_Personas(cedulas,provincias,accesorios,genero,colores_de_piel,rostro,emociones,atri_cabello,atri_ojos):
+def Crea_Personas(cedulas,provincias,vestuarios,genero,colores_de_piel,rostro,emociones,atri_cabello,atri_ojos):
     """    Function that creates a list to store persons. 
     Depending on the quantity of ID cards, the function creates persons, through a "for" loop.
     All of the attributes are randomly selected and saved in a dictionary, for each person.
@@ -299,25 +366,51 @@ def Crea_Personas(cedulas,provincias,accesorios,genero,colores_de_piel,rostro,em
     for i in range(1,len(cedulas)+1):       #The dictionary that contains ID cards, is toured
         Fecha_na = Crea_Fecha_Nac()         #The function "Crea_Fecha_Nac" is called to generate a random date of birth
         Edad_Años = Calcula_Edad(Fecha_na)  #The function "Calcula_Edad" is called to calculate the person's age
+
         cabello = {1: atri_cabello[1][random.randint(1,6)], 2: atri_cabello[2][random.randint(1,3)], 3: atri_cabello[3][random.randint(1,3)]}
         Obj_Cabello = Cabello()
         Obj_Cabello.set_Atributos_cabello({1: cabello[1], 2: cabello[2], 3: cabello[3]})
         Obj_Cabello.set_Color()
         Obj_Cabello.set_Densidad()
         Obj_Cabello.set_Textura()
+
         ojos = {1: atri_ojos[1][random.randint(1,8)], 2: atri_ojos[2][random.randint(1,7)]}
         Obj_Ojos = Ojos()
         Obj_Ojos.set_Atributos_ojos({1: ojos[1], 2: ojos[2]})
         Obj_Ojos.set_Color()
         Obj_Ojos.set_Forma()
-        dic_persona = {1: cedulas[random.randint(1,len(cedulas))], 2: provincias[random.randint(1,len(provincias))], 3: accesorios[random.randint(1,len(accesorios))],
+
+        cant_accesorios = random.randint(0,5)
+        cant_ropa = random.randint(4,7)
+        accesorios = []
+        prendas = []
+        calzado = vestuarios[3][random.randint(1,len(vestuarios[3]))]
+
+        for j in range(0,cant_accesorios):
+            accesorio = vestuarios[1][random.randint(1,len(vestuarios[1]))]
+            while accesorio in accesorios:
+                accesorio = vestuarios[1][random.randint(1,len(vestuarios[1]))]
+            accesorios.append(accesorio)
+
+        for e in range(0,cant_ropa):
+            ropa = vestuarios[2][random.randint(1,len(vestuarios[2]))]
+            while ropa in prendas:
+                ropa = vestuarios[2][random.randint(1,len(vestuarios[2]))]
+            prendas.append(ropa)
+
+        Obj_Vestuario = Vestuario()
+        Obj_Vestuario.set_Accesorios(accesorios)
+        Obj_Vestuario.set_Calzado(calzado)
+        Obj_Vestuario.set_Ropa(prendas)
+
+        dic_persona = {1: cedulas[random.randint(1,len(cedulas))], 2: provincias[random.randint(1,len(provincias))], 3: Obj_Vestuario,
                        4: genero[random.randint(1,len(genero))], 5: colores_de_piel[random.randint(1,len(colores_de_piel))],
                        6: rostro[random.randint(1,len(rostro))], 7: emociones[random.randint(1,len(emociones))],
                        8: Obj_Cabello, 9: Obj_Ojos, 10: Edad_Años} #The attributes are randomly selected and saved in a dictionary
         Lista_Personas.append(dic_persona) #It is appended each dictionary to the person's list
     return Lista_Personas
 
-def Crear_Persona_Auto(Personas,nueva_cedula,provincias,accesorios,genero,colores_de_piel,rostro,emociones,atri_cabello,atri_ojos):
+def Crear_Persona_Auto(Personas,nueva_cedula,provincias,vestuarios,genero,colores_de_piel,rostro,emociones,atri_cabello,atri_ojos):
     """    Function that creates a person and add him to the list of persons, after that, returns the list. 
     The attributes of this person are randomly selected.
 
@@ -338,18 +431,44 @@ def Crear_Persona_Auto(Personas,nueva_cedula,provincias,accesorios,genero,colore
             nueva_cedula = Crea_cedulas(1)
     Fecha_na = Crea_Fecha_Nac()                   #The function "Crea_Fecha_Nac" is called to generate a random date of birth
     Edad_Años = Calcula_Edad(Fecha_na)            #The function "Calcula_Edad" is called to calculate the person's age
+
     cabello = {1: atri_cabello[1][random.randint(1,6)], 2: atri_cabello[2][random.randint(1,3)], 3: atri_cabello[3][random.randint(1,3)]}
     Obj_Cabello = Cabello()
     Obj_Cabello.set_Atributos_cabello({1: cabello[1], 2: cabello[2], 3: cabello[3]})
     Obj_Cabello.set_Color()
     Obj_Cabello.set_Densidad()
     Obj_Cabello.set_Textura()
+
     ojos = {1: atri_ojos[1][random.randint(1,8)], 2: atri_ojos[2][random.randint(1,7)]}
     Obj_Ojos = Ojos()
     Obj_Ojos.set_Atributos_ojos({1: ojos[1], 2: ojos[2]})
     Obj_Ojos.set_Color()
     Obj_Ojos.set_Forma()
-    dic_persona = {1: nueva_cedula[1], 2: provincias[random.randint(1,len(provincias))], 3: accesorios[random.randint(1,len(accesorios))],
+
+    cant_accesorios = random.randint(0,5)
+    cant_ropa = random.randint(4,7)
+    accesorios = []
+    prendas = []
+    calzado = vestuarios[3][random.randint(1,len(vestuarios[3]))]
+
+    for j in range(0,cant_accesorios):
+        accesorio = vestuarios[1][random.randint(1,len(vestuarios[1]))]
+        while accesorio in accesorios:
+            accesorio = vestuarios[1][random.randint(1,len(vestuarios[1]))]
+        accesorios.append(accesorio)
+
+    for e in range(0,cant_ropa):
+        ropa = vestuarios[2][random.randint(1,len(vestuarios[2]))]
+        while ropa in prendas:
+            ropa = vestuarios[2][random.randint(1,len(vestuarios[2]))]
+        prendas.append(ropa)
+
+    Obj_Vestuario = Vestuario()
+    Obj_Vestuario.set_Accesorios(accesorios)
+    Obj_Vestuario.set_Calzado(calzado)
+    Obj_Vestuario.set_Ropa(prendas)
+
+    dic_persona = {1: nueva_cedula[1], 2: provincias[random.randint(1,len(provincias))], 3: Obj_Vestuario,
                    4: genero[random.randint(1,len(genero))], 5: colores_de_piel[random.randint(1,len(colores_de_piel))],
                    6: rostro[random.randint(1,len(rostro))], 7: emociones[random.randint(1,len(emociones))],
                    8: Obj_Cabello, 9: Obj_Ojos, 10: Edad_Años}  #The attributes are randomly selected and saved in a dictionary
@@ -376,9 +495,9 @@ def Administrador(Personas):
         print("*********************************************************\n")
         comando = int(input("Digite un número correspondiente con el menú: "))
         if comando == 1:                                                       #Option number 1: Create a person automatically
-            Personas = Crear_Persona_Auto(Personas,Crea_cedulas(1),Crea_provincias(),Crea_accesorios(),Crea_genero(),Crea_color_piel(),Crea_rostro(),Crea_emociones(),Crea_Atributos_Cabello(),Crea_Atributos_Ojos())
+            Personas = Crear_Persona_Auto(Personas,Crea_cedulas(1),Crea_provincias(),Crea_vestuario(),Crea_genero(),Crea_color_piel(),Crea_rostro(),Crea_emociones(),Crea_Atributos_Cabello(),Crea_Atributos_Ojos())
         elif comando == 2:                                                     #Option number 2: Create a person manually
-            Personas = Crear_Persona_Manual(Personas,Crea_cedulas(1),Crea_provincias(),Crea_accesorios(),Crea_genero(),Crea_color_piel(),Crea_rostro(),Crea_emociones(),Crea_Atributos_Cabello(),Crea_Atributos_Ojos())
+            Personas = Crear_Persona_Manual(Personas,Crea_cedulas(1),Crea_provincias(),Crea_vestuario(),Crea_genero(),Crea_color_piel(),Crea_rostro(),Crea_emociones(),Crea_Atributos_Cabello(),Crea_Atributos_Ojos())
     return Personas
 
 def Cuenta_Grupos_Etarios(Rango_edades,comando,Personas, provincia):
@@ -447,8 +566,14 @@ def Informes_Provincias(Personas):
                         print("|", Personas[i][5].get_Nombre(),"         |",Personas[i][6].get_Nombre(),"       |",Personas[i][9].get_Forma(),"    |",Personas[i][9].get_Color(),"        |")
                         print("|¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯|")
                         print("|___________________________________________________________________________|")
-                        print("| Color cabello     | Densidad cabello    | Textura cabello    | Accesorio  |")
-                        print("|", Personas[i][8].get_Color(), "        |", Personas[i][8].get_Densidad(), "          |", Personas[i][8].get_Textura(), "          |", Personas[i][3],"  |")
+                        print("| Color cabello     | Densidad cabello    | Textura cabello    | Calzado    |")
+                        print("|", Personas[i][8].get_Color(), "        |", Personas[i][8].get_Densidad(), "          |", Personas[i][8].get_Textura(), "          |", Personas[i][3].get_Calzado(),"  |")
+                        print("\n| Accesorios        |")
+                        for x in range(0,len(Personas[i][3].get_Accesorios())):
+                            print(Personas[i][3].get_Accesorios()[x].get_Accesorio())
+                        print("\n| Vestuario         |")    
+                        for y in range(0,len(Personas[i][3].get_Ropa())):    
+                            print(Personas[i][3].get_Ropa()[y].get_Ropa())
                         print(" ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯")
                         print("---------------------------------------------------------------------------------")
                         PersonasProvi += 1                                     #The quantity of persons from that province increases.
@@ -588,7 +713,7 @@ def login():
     """Is the main function, allows select as which user login."""
     comando = 0
     #The "Crea_Personas" function is called and the list that it returns is saved in a variable (Personas).
-    Personas = Crea_Personas(Crea_cedulas(170), Crea_provincias(), Crea_accesorios(),Crea_genero(), Crea_color_piel(), Crea_rostro(), Crea_emociones(),Crea_Atributos_Cabello(), Crea_Atributos_Ojos())
+    Personas = Crea_Personas(Crea_cedulas(1111), Crea_provincias(), Crea_vestuario(),Crea_genero(), Crea_color_piel(), Crea_rostro(), Crea_emociones(),Crea_Atributos_Cabello(), Crea_Atributos_Ojos())
     while comando < 3:
         print("\n*******************************************************")
         print("*                                                     *")  #There is a menu for select as which user login
